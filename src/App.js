b65530b573776c088158d620ced604278fe59fea
Michael <michaelr9wilson@gmail.com>
2022-07-03 18:52:02 -0700

    Updated form to use formik, yup and mui components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import SinglePage from "./layout/single_page/SinglePage";
 import 'uikit/dist/js/uikit.min'
 import 'uikit/dist/css/uikit.min.css'
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme/theme";
 
 function App() {
     return (
         <div className={"App"}>
-            <SinglePage/>
+            <ThemeProvider theme={theme}>
+              <SinglePage/>
+            </ThemeProvider>
         </div>
     );
 }
--- src/components/FormOMine/FormOMine.js
+++ src/components/FormOMine/FormOMine.js
@@ -1,112 +1,155 @@
 import React from 'react'
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Form, Formik } from "formik";
+import { TextField, Button, Grid } from "@mui/material";
 import Axios from "axios";
 import './FormOMine.css'
 import SectionHeader from "../SectionHeader/SectionHeader";
+import * as yup from "yup";
+import styled from "@emotion/styled";
+
+const InnerContainer = styled.div((props)=>({
+  maxWidth:420,
+  width:'100%',
+  alignSelf:"center",
+}))
+
+const yupValidators = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email : yup.string().required("Email is required"),
+  message: yup.string().required("Message is required")
+})
+
+const initialValues = {
+  name: "",
+  email: "",
+  message: "",
+}
 
 export default class formOMine extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      disabled: false,
+      emailSent: null,
+    }
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      disabled: true
+    });
+
+    Axios.post('https://portfolio-backend-e.herokuapp.com/api/email', this.state)
+      .then(res => {
+        if (res.data.success) {
+          this.setState({
             disabled: false,
-            emailSent: null,
+            emailSent: true
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false
+          });
         }
-    }
-
-
-    handleChange = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+      })
+      .catch(err => {
+        console.log(err);
 
         this.setState({
-            [name]: value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-
-        this.setState({
-            disabled: true
+          disabled: false,
+          emailSent: false
         });
+      })
 
-        Axios.post('https://portfolio-backend-e.herokuapp.com/api/email', this.state)
-            .then(res => {
-                if (res.data.success) {
-                    this.setState({
-                        disabled: false,
-                        emailSent: true
-                    });
-                } else {
-                    this.setState({
-                        disabled: false,
-                        emailSent: false
-                    });
-                }
-            })
-            .catch(err => {
-                console.log(err);
+  }
 
-                this.setState({
-                    disabled: false,
-                    emailSent: false
-                });
-            })
-
-    }
-
-    render() {
-        return (
-            <div className={"form-container"}>
-                <SectionHeader title={"contact"}/>
-                <Form className={"form"} onSubmit={this.handleSubmit}>
-                    <Form.Group className="row" >
-                        <Form.Label className={"row-label"} htmlFor="full-name">Full Name</Form.Label>
-                        <Form.Control className={"row-input"} inputMode={'text'} id="full-name" name="name" type="text" value={this.state.name}
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-
-
-                    <Form.Group className={"row"}>
-                        <Form.Label className={"row-label"} htmlFor="email">You're Email</Form.Label>
-                        <Form.Control className={"row-input"} id="email" name="email" type="email" value={this.state.email}
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-
-
-                    <Form.Group className={"row"}>
-                        <Form.Label className={"row-label"} htmlFor="message">Message</Form.Label>
-                        <Form.Control className={"row-input"} id="message" name="message" as="textarea" rows="3" value={this.state.message}
-                                      onChange={this.handleChange}/>
-                    </Form.Group>
-
-
-                    <div className={"row"}>
-                        <div className={"row-label"}>
-
-                        </div>
-                        <div className={"row-input"}>
-                            <Button  className="submit-btn" variant="primary"
-                                     type="submit" disabled={this.state.disabled}>
-                                Send an email
-                            </Button>
-                        </div>
-                    </div>
-
-
-                    {this.state.emailSent === true &&
-                    <p style={{marginTop: 'auto', marginBottom: 'auto'}} className="d-inline success-msg">Email Sent</p>}
-                    {this.state.emailSent === false &&
-                    <p style={{marginTop: 'auto', marginBottom: 'auto'}} className="d-inline err-msg">Email Not Sent</p>}
+  render() {
+    return (
+      <div className={"form-container"}>
+        <SectionHeader title={"contact"}/>
+        <InnerContainer>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={yupValidators}
+            className={"form"}
+            onSubmit={this.handleSubmit}
+            validateOnChange={false}
+            validateOnBlur={false}
+            render={(
+              {
+                values,
+                errors,
+                handleChange
+              }
+            ) => {
+              return (
+                <Form style={{padding:'1rem'}}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        name={"name"}
+                        label={"Name"}
+                        helperText={errors.name}
+                        value={values.name}
+                        error={errors.name}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        label={"Email"}
+                        name={"email"}
+                        value={values.email}
+                        error={errors.email}
+                        onChange={handleChange}
+                        helperText={errors.email}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        label={"Message"}
+                        fullWidth
+                        multiline={true}
+                        name={"message"}
+                        value={values.message}
+                        error={errors.message}
+                        onChange={handleChange}
+                        helperText={errors.message}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button
+                        type={"submit"}
+                      >
+                        Send
+                      </Button>
+                    </Grid>
+                  </Grid>
                 </Form>
-            </div>
+              )
+            }}/>
+        </InnerContainer>
+      </div>
 
-        )
-    }
+    )
+  }
 }
